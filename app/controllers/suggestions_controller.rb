1ca6bbbb4b783e32f80f93484c47421c8d0b846b
adamzermane <adamzermane92@yahoo.com>
2022-01-04 17:55:31 +0100

    footer_homepage

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,26 +1,23 @@
 class SuggestionsController < ApplicationController
-
-
-def create
-  @suggestion = Suggestion.new(suggestion_params)
-  @category = Category.find(params[:category_id])
-  @suggestion.category = @category
-  if @suggestion.save
-    redirect_to events_path(@event)
-  else
-    render "events/show"
+  def create
+    @suggestion = Suggestion.new(suggestion_params)
+    @category = Category.find(params[:category_id])
+    @suggestion.category = @category
+    if @suggestion.save
+      redirect_to events_path(@event)
+    else
+      render "events/show"
+    end
   end
-end
 
-def update
-  @suggestion = Suggestion.find(params[:id])
-  @suggestion.update(suggestion_params)
-end
+  def update
+    @suggestion = Suggestion.find(params[:id])
+    @suggestion.update(suggestion_params)
+  end
 
   private
 
-def suggestion_params
-  params.require(:suggestion).permit(:name, :vote)
-end
-
+  def suggestion_params
+    params.require(:suggestion).permit(:name, :vote)
+  end
 end
