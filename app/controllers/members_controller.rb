cfd2e2c11f8744714d4b210546eac909fd5f7a5e
Claire Marconi <claire.marconi94@gmail.com>
2022-01-04 19:05:38 +0100

    form member working, buttin join the event on show event

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,18 +1,12 @@
 class MembersController < ApplicationController
   def create
-    @member = Member.new(member_params)
-    @member.user = current_user
     @event = Event.find(params[:event_id])
+    @member = Member.new(user: current_user, event: @event)
     @member.event = @event
     if @member.save!
       redirect_to event_path(@event)
     else
       render 'events/show'
     end
-  end
-
-  private
-  def member_params
-    params.require(:member).permit()
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,5 @@ class Category < ApplicationRecord
   belongs_to :event
   has_many :suggestions
   has_many :items
-  validates :name, presence: true, uniqueness: true
+  validates :name,presence: true, uniqueness: { scope: :event_id }
 end
