fc7ff308e811c4652cfe33175223480543f0502f
Claire Marconi <claire.marconi94@gmail.com>
2022-01-05 12:07:20 +0100

    alert added to join event

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,9 +3,10 @@ class MembersController < ApplicationController
     @event = Event.find(params[:event_id])
     @member = Member.new(user: current_user, event: @event)
     @member.event = @event
-    if @member.save!
-      redirect_to event_path(@event)
+    if @member.save
+      redirect_to event_path(@event), notice: "You've just joined #{@event.title}."
     else
+      flash[:alert] = "You are already a member of #{@event.title}."
       render 'events/show'
     end
   end
