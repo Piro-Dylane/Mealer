1f0287b268c18687c64d7cb36dd04c14e83dc7c8
Dylane Piro <piro.dylan@outlook.fr>
2022-01-07 15:50:21 +0100

    skipbeforeaction

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, :only :index
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @events = Event.all
     @event = Event.new
