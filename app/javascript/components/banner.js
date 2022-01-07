56c1f3bb7648b3e5fa12b6a9a3861c155779c8e4
Dylane Piro <piro.dylan@outlook.fr>
2022-01-07 12:56:12 +0100

    show

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
+  const banner = document.querySelector("#banner-typed-text");
+  if (banner) {
+    new Typed('#banner-typed-text', {
     strings: ["What should I bring tomorrow?", "Everyone ok with pasta?", "Anyone allergic to strawberries?"],
     typeSpeed: 50,
     loop: true
-  });
+    });
+  }
 }
 
 export { loadDynamicBannerText };
