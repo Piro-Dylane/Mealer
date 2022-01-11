0e708720ea242255e37490d5b9c9efbd1e8557a6
Dylane Piro <piro.dylan@outlook.fr>
2022-01-11 12:58:08 +0100

    model controler modified

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   CATEGORIES = ["Drinks", "Starters", "Main dishes", "Deserts", "Snacks"]
 
   belongs_to :event
-  has_many :suggestions
+  has_many :suggestions, dependent: :destroy
   has_many :items
 
   validates :name, presence: true, uniqueness: { scope: :event_id }
