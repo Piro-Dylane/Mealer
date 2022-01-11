cc90b3a2d9c4ab35c2d6e22c732df737dc857350
Dylane Piro <piro.dylan@outlook.fr>
2022-01-11 12:46:23 +0100

    destroy events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :members
-  has_many :categories
-  has_one :chatroom
-  has_many :suggestions, through: :categories
-  has_many :items, through: :categories
+  has_many :categories, dependent: :destroy
+  has_one :chatroom, dependent: :destroy
+  has_many :suggestions, through: :categories, dependent: :destroy
+  has_many :items, through: :categories, dependent: :destroy
   validates :title, length: { minimum: 3, maximum: 30 }
   validates :title, :date, :time, presence: true
   has_one_attached :photo
