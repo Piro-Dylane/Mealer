da14aa11f5c5c2869f84d763ff5df02b166f18d8
Claire Marconi <claire.marconi94@gmail.com>
2022-01-12 15:05:40 +0100

    seeds wagon students

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,3 +52,43 @@ event4 = Event.create(title: "Chez Ben", date: Date.current, time: Time.now, use
 puts "Event 4 created successfully"
 Chatroom.create(event_id: event4.id)
 puts "chatroom4 created"
+
+
+pic_julien = URI.open('https://avatars.githubusercontent.com/u/49597167?v=4')
+pic_manu = URI.open('https://avatars.githubusercontent.com/u/83393274?v=4')
+pic_sophie = URI.open('https://avatars.githubusercontent.com/u/91194902?v=4')
+pic_christophe = URI.open('https://avatars.githubusercontent.com/u/55579654?v=4"')
+pic_dylane = URI.open('https://avatars.githubusercontent.com/u/32676281?v=4')
+pic_loris = URI.open('https://avatars.githubusercontent.com/u/84146888?v=4')
+pic_adam = URI.open('https://avatars.githubusercontent.com/u/61739222?v=4')
+pic_clairem = URI.open('https://avatars.githubusercontent.com/u/92371496?v=4')
+pic_aurelie = URI.open('https://ca.slack-edge.com/T02NE0241-U02KNAEMNTE-4ec3bea91b16-512')
+pic_emilie = URI.open('https://ca.slack-edge.com/T02NE0241-U02KNKK4H4L-879f4f2b9d1e-512')
+pic_erika = URI.open('https://ca.slack-edge.com/T02NE0241-U02KKUBPTMK-192e293188a2-512')
+pic_georgia = URI.open('https://ca.slack-edge.com/T02NE0241-U02KTBF92QL-38f91ad0859c-512')
+pic_guole = URI.open('https://ca.slack-edge.com/T02NE0241-U02KN2773MJ-e59339cdf2d5-512')
+pic_isabelle = URI.open('https://ca.slack-edge.com/T02NE0241-U02K48ZTUT1-24e5b0dd5ab6-512')
+pic_jessica = URI.open('https://ca.slack-edge.com/T02NE0241-U02KP8RSLTV-0becbbd9b32c-512')
+pic_lilia = URI.open(' https://ca.slack-edge.com/T02NE0241-U02KYH1KTKK-8e5e8627af12-512')
+pic_jessica = URI.open('https://ca.slack-edge.com/T02NE0241-U02KP8RSLTV-0becbbd9b32c-512')
+pic_lucas = URI.open('https://ca.slack-edge.com/T02NE0241-U02KXKLLTFT-445ed7b6e30d-512')
+pic_maewenn = URI.open('https://ca.slack-edge.com/T02NE0241-U02K8JY7YLF-f06ce77c6f7f-512')
+pic_laure = URI.open('https://ca.slack-edge.com/T02NE0241-U02KP2QHPL2-47c81530099c-512')
+pic_sarah = URI.open('https://ca.slack-edge.com/T02NE0241-U02K73C8JHM-g15687c25d4e-512')
+pic_sophiew = URI.open('https://ca.slack-edge.com/T02NE0241-U02KPK1FW1Y-b186e47fd6e6-512')
+pic_florine = URI.open('https://media-exp1.licdn.com/dms/image/C4D03AQF-Wz-dHjht0A/profile-displayphoto-shrink_200_200/0/1561457225126?e=1647475200&v=beta&t=AwX0DSTxkz-_yDfN5t06vW-h0J6jmA9qGYdBPJF8qJg')
+pic_clairew = URI.open('https://avatars.githubusercontent.com/u/91942786?v=4')
+
+
+
+
+
+julien = User.create(email: "julien@gmail.fr", password: "password", username: "Julien", allergy: "nuts")
+julien.photo.attach(io: pic_julien, filename: 'julien.png', content_type: 'image/png')
+
+manu = User.create(email: "manu@gmail.fr", password: "password", username: "Manu")
+manu.photo.attach(io: pic_manu, filename: 'manu.png', content_type: 'image/png')
+
+
+aurelie = User.create(email: "aurelie@gmail.fr", password: "password", username: "Aurélie")
+aurelie.photo.attach(io: pic_aurelie, filename: 'aurelie.png', content_type: 'image/png')
